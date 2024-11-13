2fd830bf29f04c47a7619fdae12c363da04255c4
heloisa-andrade <heloisa.andrade@picpay.com>
2024-11-13 18:38:43 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaakjfcdls

--- wireMock -desafio/wireMock-desafio/src/test/java/com/example/wireMock/_desafio/integration/RespositoriServeletIntegration.java
+++
@@ -1,9 +0,0 @@
-package com.example.wireMock._desafio.integration;
-
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@WireMockTest(httpPort = 8081)
-public class RespositoriServeletIntegration {
-}
